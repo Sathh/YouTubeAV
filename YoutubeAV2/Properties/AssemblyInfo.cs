322b454f8ab98c54db1c83710ddbee04e3612757
sathh <jozef992@hotmail.com>
2023-05-10 22:45:00 +0200

    Updated YtExplode

--- YoutubeAV2/Properties/AssemblyInfo.cs
+++ YoutubeAV2/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 //      Build Version
 //      Month
 //      Day of month
-[assembly: AssemblyVersion("1.22.9.17")]
-[assembly: AssemblyFileVersion("1.22.9.17")]
+[assembly: AssemblyVersion("1.23.5.10")]
+[assembly: AssemblyFileVersion("1.23.5.10")]
