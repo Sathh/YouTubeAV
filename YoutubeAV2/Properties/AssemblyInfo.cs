e4f9b0fe78ae20329a9e78a3ec63d1037c0876fd
Jozef <jozef992@hotmail.com>
2023-05-25 22:52:37 +0200

    Update AssemblyInfo.cs

--- YoutubeAV2/Properties/AssemblyInfo.cs
+++ YoutubeAV2/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 //      Build Version
 //      Month
 //      Day of month
-[assembly: AssemblyVersion("1.23.5.10")]
-[assembly: AssemblyFileVersion("1.23.5.10")]
+[assembly: AssemblyVersion("1.23.5.25")]
+[assembly: AssemblyFileVersion("1.23.5.25")]
