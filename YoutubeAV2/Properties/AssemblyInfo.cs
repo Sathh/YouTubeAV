a637e07ca70bebbc06fc5b5b90148d592321f279
Jozef Koczman <jozef992@hotmail.com>
2024-09-29 19:23:06 +0200

    update dependencies

--- YoutubeAV2/Properties/AssemblyInfo.cs
+++ YoutubeAV2/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 //      Build Version
 //      Month
 //      Day of month
-[assembly: AssemblyVersion("1.24.7.10")]
-[assembly: AssemblyFileVersion("1.24.7.10")]
+[assembly: AssemblyVersion("1.24.9.29")]
+[assembly: AssemblyFileVersion("1.24.9.29")]
