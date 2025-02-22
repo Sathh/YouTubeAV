f372270f747f165a53c7ebccbb9b08b7ef4b44a8
Jozef Koczman <jozef992@hotmail.com>
2025-02-22 21:49:39 +0100

    Bump dependencies

--- YoutubeAV2/Properties/AssemblyInfo.cs
+++ YoutubeAV2/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 //      Build Version
 //      Month
 //      Day of month
-[assembly: AssemblyVersion("1.25.1.26")]
-[assembly: AssemblyFileVersion("1.25.1.26")]
+[assembly: AssemblyVersion("1.25.2.22")]
+[assembly: AssemblyFileVersion("1.25.2.22")]
